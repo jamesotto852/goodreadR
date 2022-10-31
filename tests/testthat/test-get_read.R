8d5261087870ff901c91dd9746bca15378df124f
jamesotto852 <jamesotto852@gmail.com>
2022-10-31 18:12:21 +0000

    Fix test -- updated to correct no. of pages

--- tests/testthat/test-get_read.R
+++ tests/testthat/test-get_read.R
@@ -2,7 +2,8 @@ test_that("read table works", {
   df <- get_profile_url("bookononism") %>% get_read()
 
   # checksum for pages read in 2021
-  expect_equal(sum(df$pages[grep("2021", df$end_date)], na.rm = TRUE), 11212)
+  # occasionally need to update expected value, according to rereads
+  expect_equal(sum(df$pages[grep("2021", df$end_date)], na.rm = TRUE), 11007)
 
   # checking that books w/ multiple series are dealt with correctly
   # (pick only the first series)
